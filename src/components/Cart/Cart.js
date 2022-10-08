f8e460fb20adef22a9ad3f678c2153af5dac4006
webnur <mdnuruli291@gmai.com>
2022-10-08 12:54:32 +0600

    Explore Conditional Rendering & conditional css class

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart, handleRemoveItem}) => {
+    let message;
+    if(cart.length === 0){
+        message = <p>please buy at least one item!!!! </p>
+    }else if(cart.length === 1){
+        message = <div>
+            <p>amar jonno akta nao</p>
+            <p>tumar jonno arekta nao</p>
+            <p><small>amar jonno arekta nao</small></p>
+        </div>
+    }
+    else {
+        message = <p>Thanks for Buying!!!</p>
+    }
     return (
         <div>
-            <h1>Order Summery</h1>
-            <p>total product: {cart.length}</p>
+            <h1 className={cart.length === 2 ? 'orange' : 'purple'}>Order Summery</h1>
+            <p className={`bold ${cart.length ===2 ?'bule': 'purple'}`}>total product: {cart.length}</p>
 
             {
                 cart.map(tshirt => <p key={tshirt._id}>
@@ -12,8 +26,26 @@ const Cart = ({cart, handleRemoveItem}) => {
                     <button onClick={() => handleRemoveItem(tshirt)}>X</button>
                 </p>)
             }
+            {
+                message
+            }
+            <p>ternary operator</p>
+            {cart.length === 3 ? <p>tinta kinba???</p> : <p>kino kino</p>}
+            <p>and operator</p>
+            {cart.length === 2 && <h2>Double Item</h2>}
+            <p>OR Operator</p>
+            {cart.length ===4 || <p>charta nah</p>}
         </div>
     );
 };
 
 export default Cart;
+
+
+/* 
+Conditional Rendering
+1. use element in a variable and then use if-else to set value
+2. ternary operation condition ? true : false 
+3. && operator (if condition is true, i want to display something)
+4. || operator (if condition is false, i want to display something)
+*/
